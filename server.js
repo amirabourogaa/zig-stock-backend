ebf1dd5f9b652d205ec37f45be2e491993fabc51
amirabourogaa <abouragba@u-label.com>
2024-12-03 16:10:50 +0100

    Fix cors

--- server.js
+++ server.js
@@ -35,6 +35,14 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://zig-stock-front.vercel.app'); // Frontend URL
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use("/", express.static("uploads"));
 
 app.get("/test", (req, res) => {
